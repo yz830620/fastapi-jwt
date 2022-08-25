61b0bd93ab88f04e0f5be1b8c205f60256d32968
Even Pan <yz830620@gmail.com>
2022-08-26 00:13:47 +0800

    add the basic boiler plate

---
+++ app/api.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from app.model import PostSchema
+
+posts = [
+    {
+        "id": 1,
+        "title": "Pancake",
+        "content": "Lorem Ipsum ..."
+    }
+]
+
+users = []
+
+
+app = FastAPI()
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to your blog!."}
+
+
+@app.get('/posts', tags=["posts"])
+async def get_posts() -> dict:
+    return {"data": posts}
+
+
+@app.get('/posts/{id}', tags=["posts"])
+async def get_single_post(id: int) -> dict:
+    if id > len(posts):
+        return {
+            "error": "No such post with the supplied ID."
+        }
+    
+    for post in posts:
+        if post["id"] == id:
+            return {
+                "data": post
+            }
+
+@app.post('/posts', tags=["posts"])
+async def add_posts(post: PostSchema) -> dict:
+    post.id = len(posts) + 1
+    posts.append(post.dict())
+    return {
+        "data": "post added."
+    }
